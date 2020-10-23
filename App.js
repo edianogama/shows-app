1b660f4c461e83d59606854253098f727133ed27
Ediano Gama <eddy.tadim@gmail.com>
2020-10-23 13:43:44 -0300

    expo react- navigation component

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import { NavigationContainer} from '@react-navigation/native'
+import  { createStackNavigator } from '@react-navigation/stack'
+import HomeScreen from './src/screens/Home'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Séries inc" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
---
+++ README.md
@@ -0,0 +1,13 @@
+#criando APP
+``code
+$ npx expo-cli init shows-app
+``
+#Comandos para starter padrão
+``code
+expo install react-native-gesture-handler reactive-native-reanimated react-native-screens react-nativee-safe-area-context @react-native-community/masked-view
+``
+#Navigation
+``code
+$ yarn add @react-navigation/native 
+$ yarn add @react-navigation/stack
+``
