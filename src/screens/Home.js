c1e60df595242870ec4910c89adeef6950bb662c
Ediano Gama <eddy.tadim@gmail.com>
2020-10-26 18:11:23 -0300

    homeScreen name

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,79 +2,20 @@ import React, { useState } from "react";
 import  { StyleSheet, View, Text, TextInput } from "react-native";
 
 const HomeScreen = () => {
-
-
     return (
         <>
-        <View>
-            <Text></Text>
-            <Text></Text>
-            <Text>Essa é a tela de Home</Text>
+        <View style={styles.viewStyle}>
+            <Text>Séries Inc.</Text>
         </View>
         </>
-        
     )
 }
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    textInputStyle: {
-        backgroundColor: "#ddd",
-        marginTop:20,
-        padding: 10,
-        marginLeft:10,
-        marginRight:10
-    },
-    containerStyle: {
-        flex: 1,
-        margin: 10,
-    },
-    vone: {
-        flex:2,
-        flexDirection: "row",
-        justifyContent:"space-around",
-        alignItems: "flex-end",
-        backgroundColor:"green",
-     },
-    vtwo: { 
-        flex: 3,
-        backgroundColor:"orange",
-        alignItems:"flex-end",
-        color:"white",
-        flexDirection: "column",
-        justifyContent:"space-between"
-    },
     viewStyle:{
         flex: 1,
-        alignItems: "stretch",
-        justifyContent: "flex-start",
-        marginTop:10
+        alignItems: "center",
+        justifyContent: "center",
     },
-    titleStyle: {
-        fontSize:20,
-        marginBottom:10,
-
-    },
-    textUpStyle: {
-        alignSelf: "center",
-        backgroundColor:"gray",
-        margin:5,
-        overflow:"hidden",
-        padding:10,
-        borderWidth:2,
-        borderRadius: 10,
-        color:"#fff",
-        borderColor:"black"
-    },
-
-    textStyle: { 
-        backgroundColor:"gray",
-        margin:5,
-        overflow:"hidden",
-        padding:10,
-        borderWidth:2,
-        borderRadius: 10,
-        color:"#fff",
-        borderColor:"black"
-    }
 })
